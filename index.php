6fa0f31efa99de5ee55e5e5133f1a6f2c0b7ef74
Pavel Shageev <net7x@pisem.net>
2011-10-02 19:44:38 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<meta charset='utf-8'>";
+echo "<title>";
+echo "Сложный выбор - Магнитогорск";
+echo "</title>";
+echo "<link rel='stylesheet' type='text/css' href='main.css'>";
+echo "</head>";
+echo "<body>";
+?>
+
+  <div id="container">
+    <div id="header">
+      <h1>
+        Сложный выбор
+      </h1>
+    </div>
+    <div id="navigation">
+      <ul>
+        <li><a href="#">Статьи</a></li>
+        <li><a href="#">Профессии</a></li>
+        <li><a href="#">Учебные заведения</a></li>
+        <li><a href="#">Тесты</a></li>
+        <li><a href="#">Организации</a></li>
+        <li id='nav-right'><a href="#">Контакты</a></li>
+      </ul>
+    </div>
+    <div id="content-container">
+      <div id="content">
+        <h2>
+          Заголовок статьи
+        </h2>
+        <p>
+          Большинство молодых людей, размышляющих о дальнейших образовательных планах или о трудоустройстве, вообще очень мало осведомлены о том, какие бывают профессии и чем занимаются их представители. В результате ребята оказываются в ситуации выбора кота в мешке
+        </p>
+        
+      </div>
+      <div id="aside">
+        <h3>
+          Анонс
+        </h3>
+        <p>
+          Сотрудники
+        </p>
+      </div>
+      <div id="footer">
+        2011, IT project for CISCO by Shageev P
+      </div>
+    </div>
+  </div>
+<?php
+echo "</body>"; 
+echo "</html>"; 
+?>
